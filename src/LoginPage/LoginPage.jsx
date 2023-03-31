50a74c5719790c18dd12051e3ff22719f8d42f6b
Akshay <kumarakshay5042@gmail.com>
2023-03-31 15:23:07 +0530

    Login Page Done

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,4 +1,5 @@
-import  React from 'react';
+import  React from 'react'
+// import {Link } from 'react-router-dom'
 import "./LoginPage.css"
 
 function LoginPage() {
@@ -9,37 +10,37 @@ function LoginPage() {
                 facebook
             </div>
             <div className="title">
-                Facebook lets you connect and share <br /> with peoplei your life.
+                Facebook lets you connect and share <br /> with people your life.
             </div>
         </div>
-        <div className="logincontainer">
+        <div className="loginContainer">
             <div className="logindetails">
                 <input type="email" placeholder='Email address or phone number' />
                 <br />
                 <input type="password" placeholder='Password' />
                 <br />
                     
-            <button className="btn">
-            <Link to="/HeaderArea">LogIn</Link>
+                <button className="btn">
+                {/* <Link to="/HeaderArea">LogIn</Link> */}
+                Log In
                 </button>
                 </div>
-            <div className="forget">
-            <a href="forget">Forgotten account?</a>
-            <br></br>
-            </div>
-            <div className="create">
-            <br></br> 
-            <button className="btns">
-            Create New Account
-            </button>
-        </div>
-        <p></p>
-        <br></br>
-        <div className="page">
-            <a href="createpage">Create a Page </a> for a celebrity, band or business.
-        </div>
-        </div>
-        
+                <div className="forget">
+                    <a href="forget">Forgotten password?</a>
+                    <br></br>
+                </div>
+                <div className="create">
+                    <br></br> 
+                    <button className="btns">
+                        Create New Account
+                    </button>
+                </div>
+                <p></p>
+                <br></br>
+                <div className="page">
+                    <a href="createpage">Create a Page </a> for a celebrity, band or business.
+                </div>
+            </div>      
         </div>
         )
 }
